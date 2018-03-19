486f277f645add02eef70a2701ab85b7738c766e
Yingchi Pei <yingchi.pei@gmail.com>
2018-03-19 17:52:29 +0800

    add sample script

---
+++ dual_moving_average.py
@@ -0,0 +1,29 @@
+from zipline.api import (
+history,
+order_target,
+record,
+symbol,
+)
+
+def initialize(context):
+    context.i = 0
+
+def handle_data(context, data):
+    context.i += 1
+    if context.i < 300:
+        return
+
+    short_mvag = history(100, '1d', 'price').mean()
+    long_mvad = history(300, '1d', 'price').mean()
+
+    sym = symbol('AAPL')
+
+    if short_mvag[sym] > long_mvad[sym]:
+        order_target(sym, 100)
+    elif short_mvag[sym] < long_mvad[sym]:
+        order_target(sym, 0)
+
+    record(AAPL=data[sym].price,
+           short_mvag=short_mvag[sym],
+           long_mvad=long_mvad[sym])
+
